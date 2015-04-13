65fa2bac9b1a2a550773b47450c5fba8c571d12a
Mukund <mukundc@berkeley.edu>
2015-04-13 02:51:55 -0700

    features generated for hamming

--- exercism_data/feature_generation_all_submissions.py
+++ exercism_data/feature_generation_all_submissions.py
@@ -2,6 +2,7 @@ import argparse
 import subprocess
 import os
 
-
-for submission_id in range(0,1000):
-		subprocess.call(['python', '../featurization/individual_features.py', 'compute', str(submission_id), 'ruby', 'inherent_features_hamming.np', os.path.abspath("../exercism_data/"), 'flog', 'libcall', 'control_flow', 'duplicate_treegram'])
+for submission_id in range(0,986):
+		subprocess.call(['python', '../featurization/individual_features.py', 'compute', str(submission_id), 'ruby', 'hamming/feature/inherent_style_features.np', os.path.abspath("./hamming/"), 'flog'])
+		subprocess.call(['python', '../featurization/individual_features.py', 'compute', str(submission_id), 'ruby', 'hamming/feature/instrumental_style_features.np', os.path.abspath("./hamming/"), 'libcall', 'control_flow', 'duplicate_treegram'])
+  
