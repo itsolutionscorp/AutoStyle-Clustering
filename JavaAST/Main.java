c17562b0911ffdf98be27a375ff0734c029a58f1
Joseph Moghadam <jmoghadam@berkeley.edu>
2015-04-15 04:08:14 -0700

    added code to parse java asts

---
+++ JavaAST/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+ 
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+ 
+public class Main {
+ 
+	//use ASTParse to parse string
+	public static void parse(String str) {
+		ASTParser parser = ASTParser.newParser(AST.JLS4);
+		parser.setSource(str.toCharArray());
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+ 
+		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+		SimpleVisitor sv = new SimpleVisitor("encourage");
+		cu.accept(sv);
+		System.out.println(sv);
+	}
+ 
+	//read file content into a string
+	public static String readFileToString(String filePath) throws IOException {
+		StringBuilder fileData = new StringBuilder(1000);
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+ 
+		char[] buf = new char[10];
+		int numRead = 0;
+		while ((numRead = reader.read(buf)) != -1) {
+			String readData = String.valueOf(buf, 0, numRead);
+			fileData.append(readData);
+			buf = new char[1024];
+		}
+ 
+		reader.close();
+ 
+		return  fileData.toString();	
+	}
+ 
+	public static void main(String[] args) throws IOException {
+		parse(readFileToString("./Love.java"));
+	}
+}
---
+++ JavaAST/SimpleVisitor.java
@@ -0,0 +1,106 @@
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ForStatement;
+import org.eclipse.jdt.core.dom.IfStatement;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+
+public class SimpleVisitor extends ASTVisitor {
+	private SimpleASTNode root;
+	private SimpleASTNode current;
+	private String methodOfInterest;
+	private boolean inMethodOfInterest;
+	
+	public SimpleVisitor(String methodName){
+		root = null;
+		current = null;
+		methodOfInterest = methodName;
+		inMethodOfInterest = false;
+	}
+	
+	@Override
+	public void preVisit(ASTNode n){
+	}
+	
+	@Override
+	public void postVisit(ASTNode n){
+	}
+	
+	@Override
+	public boolean visit(MethodDeclaration n){
+		if (n.getName().toString().equals(methodOfInterest)){
+			root = new SimpleASTNode(methodOfInterest);
+			current = root;
+			inMethodOfInterest = true;
+		}
+		return true;
+	}
+	
+	@Override
+	public void endVisit(MethodDeclaration n){
+		if (n.getName().toString().equals(methodOfInterest)){
+			System.out.print("done with ");
+			System.out.println(n);
+			inMethodOfInterest = false;
+		}
+	}
+	
+	@Override
+	public boolean visit(MethodInvocation n){
+		return visitItem(n.getName().toString());
+	}
+	
+	private void endVisitItem(){
+		if (inMethodOfInterest && current!=null){
+			current = current.getParent();
+		}
+	}
+	
+	@Override
+	public void endVisit(MethodInvocation n){
+		endVisitItem();
+	}
+	
+	@Override
+	public boolean visit(ForStatement n){
+		return visitItem("iter");
+	}
+	
+	@Override
+	public void endVisit(ForStatement n){
+		endVisitItem();
+	}
+	
+	@Override
+	public boolean visit(IfStatement n){
+		return visitItem("cond");
+	}
+	
+	@Override
+	public void endVisit(IfStatement n){
+		endVisitItem();
+	}
+	
+	@Override
+	public String toString(){
+		return root.toString();
+	}
+	
+	private boolean visitItem(String item){
+		if (inMethodOfInterest){
+			System.out.println(item);
+		}
+		if (!inMethodOfInterest){
+			return true;
+		} else if (current != null){
+			System.out.println(item);
+			SimpleASTNode thisItem = new SimpleASTNode(item);
+			current.addChild(thisItem);
+			thisItem.setParent(current);
+			current = thisItem;
+			return true;
+		} else {
+			return true;
+		}
+	}
+}
